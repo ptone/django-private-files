bfdf17121cc64d6bffedd1a9b2f6c854af70ce14
Vasil Vangelovski <vvangelovski@gmail.com>
2011-03-24 03:00:37 +0100

    version bump; long description read from README.rst

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ except ImportError:
     
 setup(
     name = 'django-private-files',
-    version = '0.1.0',
+    version = '0.1.1',
     packages = ['private_files', 'private_files.models'],
     author = 'Vasil Vangelovski',
     author_email = 'vvangelovski@gmail.com',
     license = 'New BSD License (http://www.opensource.org/licenses/bsd-license.php)',
     description = 'Protected files in django',
-    long_description = "A reusable django app that provides facilities for easy control over static file permissions.",
+    long_description = open("README.rst").read(),
     url = 'https://bitbucket.org/vvangelovski/django-private-files',
-    download_url = 'https://bitbucket.org/vvangelovski/django-private-files/get/v0.1.0.tar.gz',
+    download_url = 'https://bitbucket.org/vvangelovski/django-private-files/downloads',
     include_package_data = True,
     
     classifiers=[
